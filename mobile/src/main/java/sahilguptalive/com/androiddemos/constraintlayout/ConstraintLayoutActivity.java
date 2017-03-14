52dd5126f837bfb87a3fe5c0e62d9a69f5a2f1f2
Sahil Gupta <sahil.gupta@nagarro.com>
2017-03-14 12:59:01 +0530

    added complimentary project

---
+++ mobile/src/main/java/sahilguptalive/com/androiddemos/constraintlayout/ConstraintLayoutActivity.java
@@ -0,0 +1,20 @@
+package sahilguptalive.com.androiddemos.constraintlayout;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import sahilguptalive.com.androiddemos.R;
+
+/**
+ * Created by sahilgupta on 07-03-2017.
+ */
+
+public class ConstraintLayoutActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.constraint_layout);
+    }
+}
